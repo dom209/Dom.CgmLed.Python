ae1201223f2d7646ffcf7bed05ae309fb508c065
Dominic Reid <dominic.reid@dmar.me.uk>
2018-07-02 23:46:32 +0100

    Added Pi debugging.

--- CgmLed/CgmLed.py
+++ CgmLed/CgmLed.py
@@ -1,1 +1,14 @@
 
+import ptvsd
+
+ptvsd.enable_attach(secret='my_secret')
+
+ 
+
+first_num = int(raw_input('Enter first number: '))
+
+second_num = int(raw_input('Enter second number: '))
+
+sum = first_num + second_num
+
+print('{0} + {1} = {2}: '.format(first_num, second_num, sum))
